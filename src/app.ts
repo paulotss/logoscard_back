68d10383c00e60e08b6c89ac5baa9ea8716e496d
Paulo de Tarso <paulo.oinab@gmail.com>
2023-06-16 17:15:02 -0300

    apply cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import userRouter from './routes/user.route';
 import planRouter from './routes/plan.route';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use(userRouter);
 app.use(planRouter);
 
