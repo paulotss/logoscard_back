8ffb5e8e05600e1eda0e6da1d507c0cfa9971705
Paulo de Tarso <paulo.oinab@gmail.com>
2023-07-21 14:50:54 -0300

    create AdminModel

---
+++ src/database/models/admin.model.ts
@@ -0,0 +1,40 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import UserModel from './user.model';
+
+class AdminModel extends Model {
+  declare id: number;
+
+  declare userId: number;
+}
+
+AdminModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: {
+          tableName: 'users',
+        },
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize: db,
+    underscored: true,
+    timestamps: false,
+    modelName: 'AdminModel',
+    tableName: 'admins',
+  },
+);
+
+AdminModel.belongsTo(UserModel);
+
+export default AdminModel;
