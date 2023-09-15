b482cf155068eae59b0515a7abf854deed05ebef
Paulo de Tarso <paulo.oinab@gmail.com>
2023-09-15 15:07:01 -0300

    reactoring controller and route

--- src/routes/invoice.route.ts
+++ src/routes/invoice.route.ts
@@ -7,23 +7,23 @@ router.get('/invoice/:id', (req, res, next) =>
   new InvoiceController(req, res, next).getOne(),
 );
 
-router.put('/invoice/pay', (req, res, next) =>
-  new InvoiceController(req, res, next).pay(),
+router.put('/invoice/update', (req, res, next) =>
+  new InvoiceController(req, res, next).update(),
 );
 
 router.post('/invoices', (req, res, next) =>
   new InvoiceController(req, res, next).generateInvoices(),
 );
 
-router.get('/invoices/total/paid', (req, res, next) =>
+router.get('/invoices/paid', (req, res, next) =>
   new InvoiceController(req, res, next).getTotalPaid(),
 );
 
-router.get('/invoices/total/pending', (req, res, next) =>
+router.get('/invoices/pending', (req, res, next) =>
   new InvoiceController(req, res, next).getTotalPending(),
 );
 
-router.get('/invoices/total/overdue', (req, res, next) =>
+router.get('/invoices/overdue', (req, res, next) =>
   new InvoiceController(req, res, next).getTotalOverdue(),
 );
 
