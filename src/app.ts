a709d261267ae221e56dba6f950dd5d23c143155
GGVidal <gabrielvidalgondim@gmail.com>
2025-07-15 19:48:14 -0300

    chore: fix to make it work

--- src/app.ts
+++ src/app.ts
@@ -10,16 +10,9 @@ import clientRouter from './routes/client.route';
 import dependentRouter from './routes/dependent.route';
 import depositRouter from './routes/deposit.route';
 import withdrawRouter from './routes/withdraw.route';
-import pagBankRouter from './routes/pagBank.route'
+import pagBankRouter from './routes/pagBank.route';
 
 const app = express();
-
-app.use(express.raw({
-    type: 'application/json',
-    verify: (req, res, buf) => {
-      (req as any).rawBody = buf.toString();
-    }
-  }));
 
 app.use(express.json());
 app.use(cors());
