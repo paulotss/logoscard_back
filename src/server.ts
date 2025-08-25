cb329138d35d28b6f801d3130cb832a97c9b4567
AlexandreFB2003 <afborges2003@gmail.com>
2025-08-25 14:57:59 -0300

    config development settings

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,17 @@ dotenv.config({
 });
 
 import app from './app';
+import sequelize from './database/models';
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Connection to the database has been established successfully');
+  } catch (error) {
+    console.error('❌ Unable to connect to the database:', error);
+  }
+
+  console.log(`Listening on port ${PORT}`);
+});
